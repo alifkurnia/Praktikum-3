700eeb653efab1c664233801be5d6bfe25eab170
alifkurnia <alifkurnia4@gmail.com>
2019-10-09 08:23:41 +0700

    Praktikum 3

--- app/src/main/java/com/example/praktikum3/MainActivity.java
+++ app/src/main/java/com/example/praktikum3/MainActivity.java
@@ -1,25 +1,37 @@
 package com.example.praktikum3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mBtn_login, mBtn_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = (Button) findViewById(R.id.showToast);
-// Show toast message when button is clicked
-        button.setOnClickListener(new View.OnClickListener() {
+        mBtn_login = (Button)findViewById(R.id.btn_login);
+        mBtn_register=(Button)findViewById(R.id.btn_register);
+        mBtn_login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Ini adalah pesan Toast",
-                        Toast.LENGTH_LONG).show();// Set your own toast message
+            public void onClick(View v) {
+                Intent iLogin = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(iLogin);
             }
         });
-    }}
+        mBtn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iRegister = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(iRegister);
+            }
+        });
+    }
+}
